6385b1292a76d6c3c609cc7850adc67d76f01fb8
nky163 <nky8888163@gmail.com>
2024-12-01 18:12:34 +0900

    フロントエンド追加、S3、CloudFront

--- cdk/lib/app-stack.ts
+++ cdk/lib/app-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
@@ -8,17 +12,47 @@ export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Docker イメージから Lambda 関数を作成
-    const lambdaFunction = new lambda.DockerImageFunction(this, 'DockerLambda', {
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../app')),
-      architecture: lambda.Architecture.ARM_64,
-      timeout: cdk.Duration.seconds(15), // タイムアウトを 15 秒に設定
-      memorySize: 1024, // メモリを増加
+    // S3 バケット
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // パブリックアクセスを無効化
     });
 
-    // API Gateway を作成して Lambda を公開
-    new apigateway.LambdaRestApi(this, 'ApiGateway', {
-      handler: lambdaFunction,
+    // CloudFront OAI を作成
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity');
+    siteBucket.grantRead(originAccessIdentity)
+
+    // CloudFront ディストリビューション
+    const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(siteBucket, {
+          originAccessIdentity,
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
     });
+
+    // フロントエンドファイルを S3 にデプロイ
+    new s3deploy.BucketDeployment(this, 'DeploySite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../frontend/dist'))],
+      destinationBucket: siteBucket,
+      distribution,
+    });
+
+    // Lambda 関数
+    const backendLambda = new lambda.DockerImageFunction(this, 'BackendLambda', {
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../backend')),
+      architecture: lambda.Architecture.ARM_64,
+    });
+
+    // API Gateway
+    const api = new apigateway.LambdaRestApi(this, 'BackendApi', {
+      handler: backendLambda,
+      proxy: true,
+    });
+
+    // CloudFront に API Gateway を追加
+    distribution.addBehavior('/api/*', new origins.HttpOrigin(`${api.restApiId}.execute-api.${this.region}.amazonaws.com`, {
+      originPath: '/prod',
+    }));
   }
 }
