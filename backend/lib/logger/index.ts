52e6c189c82f8bf273a47b1b65c488298b8a315d
nky163 <nky8888163@gmail.com>
2025-02-06 08:07:05 +0900

    Logger

--- backend/lib/logger/index.ts
+++ backend/lib/logger/index.ts
@@ -1,36 +1,45 @@
 // src/logger.ts
-import pino, { LoggerOptions } from 'pino';
+import pino from 'pino';
 
-// Pinoの設定オプションを自由にカスタマイズできます。
-const logger = pino({
-  // ログレベル（環境変数などで切り替えられるようにしておく例）
-  level: process.env.LOG_LEVEL || 'info',
-
-  // 時刻フォーマットの例（ISO形式など）
-  timestamp: pino.stdTimeFunctions.isoTime,
-
-  // ログ出力のフォーマット設定など
-  formatters: {
-    level(label: string) {
-      // デフォルトでは { level: 30 } のように数値が入りますが、任意で上書き可能
-      return { level: label };
+class Logger {
+  private static baseLogger = pino({
+    // ログレベル（環境変数などで切り替えられるようにしておく例）
+    level: process.env.LOG_LEVEL || 'info',
+  
+    // 時刻フォーマットの例（ISO形式など）
+    timestamp: pino.stdTimeFunctions.isoTime,
+  
+    // ログ出力のフォーマット設定など
+    formatters: {
+      level(label: string) {
+        // デフォルトでは { level: 30 } のように数値が入りますが、任意で上書き可能
+        return { level: label };
+      },
     },
-  },
+    
+    base: null,
+    
+    // 開発環境でだけpino-prettyを利用したい場合の例
+    transport:
+      process.env.NODE_ENV === 'local'
+        ? {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+              translateTime: 'SYS:standard',
+              ignore: 'pid,hostname', // 不要な項目をログから除外
+            },
+          }
+        : undefined,
+  });
   
-  base: null,
+  private static logger = Logger.baseLogger;
   
-  // 開発環境でだけpino-prettyを利用したい場合の例
-  transport:
-    process.env.NODE_ENV === 'local'
-      ? {
-          target: 'pino-pretty',
-          options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname', // 不要な項目をログから除外
-          },
-        }
-      : undefined,
-});
-
-export default logger;
+  static initialiseLogger = (bindings: pino.Bindings) => {
+    Logger.logger = Logger.baseLogger.child(bindings);
+  }
+  static getLogger = () => Logger.logger;
+  
+}
+export const initialiseLogger = Logger.initialiseLogger;
+export const getLogger = Logger.getLogger;
